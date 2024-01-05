f91a153249f05a5ea9c85594e8b736f7776b2174
xq <roachsinai@qq.com>
2024-01-05 16:08:44 +0800

    更新算法解释说明。

--- companies/dog/hill_watch.cpp
+++ companies/dog/hill_watch.cpp
@@ -1,8 +1,8 @@
-// http://codeforces.com/contest/5/problem/E
+// [E. Bindian Signalizing](http://codeforces.com/contest/5/problem/E)
 
 // 多个烽火台围成一圈，两个烽火台可以看见，当且仅当，它们之间没有高于任一方的烽火台，求可以看见的烽火台的对数？
 // 统计对数，为了不重复统计。这里只统计低的烽火台看见高的烽火台这样的对数
-// 这就需要统计对于一个烽火台i，它左边第一个高于它的烽火台位置；它右边高于它的烽火台的位置；以及，这之间合它高度相同的烽火台的位置。
+// 这就需要统计对于一个烽火台i，它左边第一个高于它的烽火台位置；它右边高于它的烽火台的位置；以及，这之间和它高度相同的烽火台的位置。
 // 之所以统计这些是：
 //      拦截效果，出现一个高于它的烽火台，之后的就看不到了；
 //      高度相同的可以互相看见；
@@ -32,18 +32,18 @@ int main()
     num.reserve(n);
     copy(height.begin(), highest, num.end());
 
-    // left[i] is the nearest bigger number in the left of num[i]
+    // left 中存放的是 num[i] 左边 nearest bigger number 的索引
     vector<int> left(n, -1);
     for (int i = 1; i < n; ++ i)
     {
         int p = i-1;
-        while (num[i] > num[p])
+        while (num[i] > num[p])  // 因为最大的元素在左端， p 不会越界
             p = left[p];
         left[i] = num[i] == num[p] ? left[p] : p;
     }
 
-    //right[i] is the nearest bigger number in the right of num[i]
-    //same[i] is the number of the number equal to num[i] in [i, right[i]]
+    // right[i] is the nearest bigger number in the right of num[i]
+    // same[i] is the number of the number equal to num[i] in [i, right[i]]
     vector<int> right(n, n);
     vector<int> same(n, 0);
     for (int i = n-2; i >= 0; -- i)
@@ -60,14 +60,16 @@ int main()
 
         right[i] = num[i] == num[p] ? right[p] : p;
         same[i] = num[i] == num[p] ? same[i] + 1 : 0;
-
     }
 
     long long ans = 0;
     for (int i = 0; i < n; ++ i) {
-        if (left[i] >= 0) ans += 1;
-        if (right[i] < n) ans += 1;
-        if (right[i] >= n && left[i] > 0) ans += 1; // 最后一个元素肯定可以看见第一个元素
+        if (left[i] >= 0)
+            ans += 1;
+        if (right[i] < n)
+            ans += 1;
+        if (right[i] >= n && left[i] > 0)
+            ans += 1;  // 最后一个元素肯定可以看见第一个元素
         ans += same[i];
     }
 
